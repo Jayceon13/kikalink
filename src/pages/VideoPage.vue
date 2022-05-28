f411a4ede4cf0dfa88da5d99476c8b564750b695
Jayceon <jayceon310893@gmail.com>
2022-05-28 10:58:03 +0300

    youtube video

--- src/pages/VideoPage.vue
+++ src/pages/VideoPage.vue
@@ -1,11 +1,26 @@
 <template>
   <q-page padding>
-    <div class="text-lg">Test</div>
+    <div class="video-youtube">
+      <iframe
+        width="300"
+        height="500"
+        src="https://youtube.com/embed/pRstof8s8y8?feature=share"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
   // name: 'PageName',
-}
+};
 </script>
+
+<style scoped>
+.video-youtube {
+  display: flex;
+  justify-content: center;
+}
+</style>
