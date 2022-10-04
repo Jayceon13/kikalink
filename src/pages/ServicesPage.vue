5dc98538564ed9eb2c65be99571390f50b63a770
Jayceon <jayceon310893@gmail.com>
2022-10-04 18:36:19 +0300

    Service page

--- src/pages/ServicesPage.vue
+++ src/pages/ServicesPage.vue
@@ -1,7 +1,62 @@
 <template>
   <q-page padding>
-    <div class="test">Test</div>
+    <div class="brow">
+      <img src="https://drive.google.com/uc?export=view&id=1gM9CPKbkvr4LJqXyKYdn21iQ249_mSno">
+    <br>
+      -Архитектура бровей /создание формы<br>
+      -Окрашивание /коррекция бровей (хна /краска)<br>
+      -Осветление бровей<br>
+      -Прореживание<br>
+      -Ламинирование ресниц<br>
+      -Протеиновое восстановление ресниц /бровей<br>
+      -Долговременная укладка бровей<br>
+      <br>
+      💔Комбо за 45 минут<br>
+      <br>
+      Ресницы + брови Botex в подарок 🎁<br>
+      <br>
+      💔Образ за 1,5 часа<br>
+      Макияж любой сложности<br>
+      Укладка / причёска<br>
+      Макияж + укладка<br>
+      Макияж тела ( грудь/ноги/руки)<br>
+      Грим<br>
+      <br>
+      <q-btn
+        href="https://api.whatsapp.com/send?phone=79811853911&text="
+        class="my-btn"
+        outline
+      >ЗАПИСАТЬСЯ</q-btn
+      >
+      <br>
+      <img src="https://drive.google.com/uc?export=view&id=1C1g2dq4Gs0krckemNtc1wiva7M-sFnhB">
+      <br>
+      -Architecture of eyebrows / shaping<br>
+      - Coloring / correction of eyebrows (henna / dye<br>
+      - Eyebrow lightening<br>
+      -thinning<br>
+      - Eyelash lamination<br>
+      -Protein restoration of eyelashes / eyebrows<br>
+      - Long lasting brow styling<br>
+      <br>
+      💔Combo in 45 minutes<br>
+      <br>
+      Eyelashes + eyebrows Botex as a gift 🎁<br>
+      <br>
+      💔 Look in 1.5 hours<br>
+      Makeup of any complexity<br>
+      Styling / hairstyle<br>
+      Makeup + styling<br>
+      <br>
+      <q-btn
+        href="https://api.whatsapp.com/send?phone=79811853911&text="
+        class="my-btn"
+        outline
+      >SING UP</q-btn
+      >
+    </div>
   </q-page>
+
 </template>
 
 <script>
@@ -9,3 +64,20 @@ export default {
   // name: 'PageName',
 }
 </script>
+
+<style scoped>
+.brow {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px;
+}
+
+.brow img {
+  width: 300px;
+  height: 50%;
+}
+
+</style>
