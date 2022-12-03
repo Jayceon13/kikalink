195d2ee68b100a78fa5f9f6ae3a20b943fe4a2a9
Андрей Алферук <65571204+Dyuk13@users.noreply.github.com>
2022-12-03 23:25:06 +0300

    No scroll

--- src/layouts/MenuLayout.vue
+++ src/layouts/MenuLayout.vue
@@ -1,12 +1,12 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <div class="menu" v-scroll style="position: fixed; z-index: 9999; ">
+  <q-layout view="hHh lpR fFf" style="font-size: 10px">
+    <div class="menu" v-scroll style="position: absolute; ">
       <block-burger v-model:show="showBurgerMenu"></block-burger>
     </div>
-    <q-header elevated class="bg-primary text-white" style="">
+    <q-header elevated class="bg-primary text-white" >
         <q-btn dense flat round icon="menu" @click="blockBurgerMenu" style=" top: 0;"/>
     </q-header>
-    <div class="page">
+    <div class="page" v-show="value1" style="background: #31ccec">
       <h1>dwda</h1>
       <h1>dwda</h1>
       <h1>dwda</h1>
@@ -22,28 +22,38 @@
 
 <style lang="scss">
 
-
+.my-btn {
+  font-family: 'PT Sans', sans-serif;
+}
 .my-btn-contact {
   margin-top: auto;
 }
+
 </style>
 
 <script>
 import BlockBurger from "components/burgerMenu";
 import { defineComponent } from "vue";
+import BlockPage from "pages/IndexPage";
+// let a =
 
 export default defineComponent({
   components: {BlockBurger,},
   data: () => ({
       showBurgerMenu: false,
+    value1: true,
   }),
   BlockBurger,
   methods: {
     blockBurgerMenu() {
       this.showBurgerMenu = !this.showBurgerMenu
+      this.value1 = !this.value1
     }
   },
-})
+
+});
+
+
 
 
 </script>
