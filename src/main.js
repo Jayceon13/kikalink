2c90e5f0c1e2b84c708d993fa122c66f986ddfbc
Jayceon <jayceon310893@gmail.com>
2022-12-09 10:51:03 +0300

    test background video mobile

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import store from "./store";
 import Axios from "axios";
+import Vue from 'vue'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// importing AOS css style globally
+import 'aos/dist/aos.css'
+
+const app = createApp(App);
+
+app.mount('#app')
+
 
 Vue.prototype.$http = Axios;
 const token = localStorage.getItem("token");
